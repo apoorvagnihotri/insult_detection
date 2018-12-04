39a2671d16b6b060a80d4355a19d6a71496da870
Apoorv <apoorvagni@hotmail.com>
2018-12-04 20:58:04 +0530

    updated GUI to include names.

--- gui/demo.py
+++ gui/demo.py
@@ -1,7 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QAction, QLineEdit, QMessageBox
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 
 def percent_insult(sentence):
     return len(sentence)
@@ -11,24 +11,45 @@ class App(QMainWindow):
     def __init__(self):
         super().__init__()
         self.title = 'Insult Detection demo | Credits - pythonspot.com'
-        self.left = 10
-        self.top = 10
-        self.width = 400
-        self.height = 140
+        self.left = 400
+        self.top = 400
+        self.width = 1050
+        self.height = 220
         self.initUI()
  
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
- 
+        
+        # Height
+        a = 80
+        wi = 1000
+        font = QFont("Times", 20, QFont.Bold) 
+
+        # Create a Parent Label
+        self.label = QLabel(self)
+        self.label.setText("Insult Detection of Social Comentary")
+        self.label.resize(wi,40)
+        self.label.setFont(font)
+        self.label.move(20, 20)
+
+        # Create a main Label
+        self.label_names = QLabel(self)
+        self.label_names.setText("Team Members: Abhavya | Apoorv"
+                                 " | Atishay | Ritik | Varun | Rithwik")
+        font = QFont("Times", 14) 
+        self.label_names.setFont(font)
+        self.label_names.resize(wi,40)
+        self.label_names.move(20, 55)
+
         # Create textbox
         self.textbox = QLineEdit(self)
-        self.textbox.move(20, 20)
-        self.textbox.resize(280,40)
+        self.textbox.move(20, a + 20)
+        self.textbox.resize(wi,40)
  
         # Create a button in the window
-        self.button = QPushButton('Show text', self)
-        self.button.move(20,80)
+        self.button = QPushButton('Calc. Porbability', self)
+        self.button.move(wi-100, a + 80)
  
         # connect button to function on_click
         self.button.clicked.connect(self.on_click)
@@ -44,6 +65,8 @@ class App(QMainWindow):
  
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    # print(app)
+    # print(type(app))
     ex = App()
     sys.exit(app.exec_())
 
